43e4e6dc8ed8b1170a365ff14344a823b56f072a
Rishish Aryal <pokhrelashok2@gmail.com>
2020-07-02 16:10:36 +0545

    Quotation ui and controller made

--- resources/views/admin/quotation/index.blade.php
+++ resources/views/admin/quotation/index.blade.php
@@ -47,7 +47,7 @@
 		<div class="col-lg-12">
 			<div class="ibox ">
 				<div class="ibox-title">
-                <h5>Search Results ({{$cleanings->count()}})</h5>
+                <h5>Search Results</h5>
 					<div class="ibox-tools">
 						<a class="collapse-link">
 							<i class="fa fa-chevron-up"></i>
